baaf2378c467b86d39b42199e3526b625c7fa202
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-03 18:06:27 +0600

    Left side navigation added category link

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch('categories.json')
+      .then((res) => res.json())
+      .catch((data) => setCategories(data));
+  }, []);
   return (
-    <div>
+    <div className="space-y-6">
       <h2 className="text-2xl">Left Side</h2>
+      {categories.map((category) => (
+        <Link
+          to={`/category/${category.id}`}
+          className="block ml-4 text-xl font-semibold"
+          key={category.id}
+        >
+          {category.name}
+        </Link>
+      ))}
     </div>
   );
 };
