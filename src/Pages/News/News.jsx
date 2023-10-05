0e0e840059fc21004b0710d371b84eea103b31b5
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-05 17:30:27 +0600

    Create Private route and do not redirect on reload

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,5 +1,22 @@
+import Header from '../Shared/Header/Header';
+import Navbar from '../Shared/Navbar/Navbar';
+import RightSideNav from '../Shared/RightSideNav/RightSideNav';
+
 const News = () => {
-  return <div>News detais</div>;
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+      <div className="grid md:grid-cols-4">
+        <div className="col-span-3">
+          <h2 className="text-5xl">hello</h2>
+        </div>
+        <div>
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default News;
